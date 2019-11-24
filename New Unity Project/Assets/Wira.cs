39b67771e7c6048cb73befed10c6bd39b5ad7aa3
EmperorRise <jimmyelan@gmail.com>
2019-11-24 12:55:06 +0700

    ini wira

---
+++ New Unity Project/Assets/Wira.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Wira : MonoBehaviour
+{
+    public Text numb1txt;
+    public Text numb2txt;
+    public Text totaltxt;
+    public int numb1;
+    public int numb2;
+    public int total;
+    public Button Tambah;
+    public Button Kurang;
+    public Button Bagi;
+    public Button Kali;
+    public Button Hasil;
+    public Button Clear;
+    public Button Up1;
+    public Button Down1;
+    public Button Up2;
+    public Button Down2;
+    void Start()
+    {
+       
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+             
+
+    }
+    public void TambahAngka1()
+    {
+        numb1 = numb1 + 1;
+       numb1txt.text = numb1.ToString();
+
+    }
+    public void KurangAngka1()
+    {
+        numb1 = numb1 - 1;
+        numb1txt.text = numb1.ToString();
+
+    }
+    public void TambahAngka2()
+    {
+        numb2 = numb2 + 1;
+        numb2txt.text = numb2.ToString();
+
+    }
+    public void KurangAngka2()
+    {
+        numb2 = numb2 - 1;
+        numb2txt.text = numb2.ToString();
+
+    }
+    public void TambahButton()
+    {
+        total = numb1 + numb2;
+        totaltxt.text = total.ToString();
+    }
+}
